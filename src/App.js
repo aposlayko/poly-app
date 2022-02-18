4ade21e530a87b418d0e3d3589fccd222d4993a7
Alexandr Poslayko <aposlayko@gmail.com>
2022-02-18 09:47:22 +0200

    hello world

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
           <Image centered size='tiny' src={iconDevx} style={{marginBottom:"2em"}}/>
           <p>Create React Ethereum DApp on Polygon</p>
           <p>Build your first app with Starter Kits</p>
+          <h1>Hello world!</h1>
         </Container>
       </Content>
       <Footer/>
